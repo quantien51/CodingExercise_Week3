3185557827228359e2f47bb1c45b3ae7fff82bfc
trannguyenphuongps@gmail.com <trannguyenphuongps@gmail.com>
2026-03-28 19:02:04 +0700

    Hoàn thành hàm giải phương trình bậc nhất và nhập xuất dữ liệu

--- CodingExercise_Week3/Equation.cpp
+++ CodingExercise_Week3/Equation.cpp
@@ -2,8 +2,26 @@
 #include <cmath>
 
 int EqFirstDeg(double a, double b, double& x) {
-    //
-    return 0;
+	int nSolution;
+	if (a != 0)
+	{
+		x = -b / a;
+		nSolution = 1;
+	}
+	else
+	{
+		x = 0;
+		if (b == 0)
+		{
+			nSolution = Undetermined;
+		}
+		else
+		{
+			nSolution = NoSolution;
+		}
+	}
+	return nSolution;
+    
 }
 
 int EqSecDeg(double a, double b, double c, double& x1, double& x2) {
